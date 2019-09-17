4c0b595e90cf9c99eb8ac2a96455df8f3102868b
iproman <itechman@yandex.ru>
2019-09-17 18:38:54 +0500

    Add footer from template.

--- wp-content/themes/clean/footer.php
+++ wp-content/themes/clean/footer.php
@@ -1,35 +1,36 @@
-<?php
-/**
- * The template for displaying the footer
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package clean
- */
+</div>
+<footer id="fh5co-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1 text-center">
+                <p>&copy; Clean. All Rights Reserved.<br>
+                    Created by <a target="_blank" rel="nofollow noopener" href="//freehtml5.co/">FreeHTML5.co</a>
+                    Images: Pexels</p>
+            </div>
+        </div>
+    </div>
+</footer>
 
-?>
+<?php if (false): ?>
+    </div><!-- #content -->
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'clean' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'clean' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'clean' ), 'clean', '<a href="https://github.com/iproman">iproman</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
+    <footer id="colophon" class="site-footer">
+        <div class="site-info">
+            <a href="<?php echo esc_url(__('https://wordpress.org/', 'clean')); ?>">
+                <?php
+                /* translators: %s: CMS name, i.e. WordPress. */
+                printf(esc_html__('Proudly powered by %s', 'clean'), 'WordPress');
+                ?>
+            </a>
+            <span class="sep"> | </span>
+            <?php
+            /* translators: 1: Theme name, 2: Theme author. */
+            printf(esc_html__('Theme: %1$s by %2$s.', 'clean'), 'clean', '<a href="https://github.com/iproman">iproman</a>');
+            ?>
+        </div><!-- .site-info -->
+    </footer><!-- #colophon -->
+    </div><!-- #page -->
+<?php endif; ?>
 <?php wp_footer(); ?>
 
 </body>
