9ff1f737a4fb0f43d49a6a001457197e5b026dc7
iproman <itechman@yandex.ru>
2019-09-19 19:28:34 +0500

    Add portfolio section.

--- wp-content/themes/clean/index.php
+++ wp-content/themes/clean/index.php
@@ -1,58 +1,33 @@
 <?php
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package clean
- */
 
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+    <div id="fh5co-portfolio">
+        <?php $query = new WP_Query([
+            'category_name' => 'home',
+        ]) ?>
+        <?php if ($query->have_posts()) : $i = 1;
+            while ($query->have_posts()) : $query->the_post(); ?>
+                <?php if (has_post_thumbnail()) $img_url = get_the_post_thumbnail_url();
+                else $img_url = 'https://picsum.photos/1280/864'; ?>
 
-		<?php
-		if ( have_posts() ) :
+                <div class="fh5co-portfolio-item <?= $i % 2 == 0 ? 'fh5co-img-right' : '' ?>">
+                    <div class="fh5co-portfolio-figure animate-box"
+                         style="background-image: url(<?= $img_url ?>);"></div>
+                    <div class="fh5co-portfolio-description">
+                        <h2><?php the_title() ?></h2>
+                        <p><?php the_content(''); ?></p>
+                        <p><a href="<?php the_permalink(); ?>"
+                              class="btn btn-primary"><?php _e('Read more', 'clean') ?></a></p>
+                    </div>
+                </div>
+                <?php $i++; endwhile; ?>
+        <?php else: ?>
+        <?php endif; ?>
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+        <?php wp_reset_postdata(); ?>
+    </div>
 
 <?php
 get_footer();
