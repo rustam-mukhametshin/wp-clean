b07beeeceb2a1c55fd865b948b0191ce9eec7d09
iproman <itechman@yandex.ru>
2019-09-20 18:04:18 +0500

    Default content in front-page.

--- wp-content/themes/clean/front-page.php
+++ wp-content/themes/clean/front-page.php
@@ -29,5 +29,13 @@ get_header();
         <?php wp_reset_postdata(); ?>
     </div>
 <?php endif; ?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <?php the_content() ?>
+<?php endwhile; ?>
+    <!-- post navigation -->
+<?php else: ?>
+    <!-- no post found -->
+<?php endif; ?>
 <?php
 get_footer();
