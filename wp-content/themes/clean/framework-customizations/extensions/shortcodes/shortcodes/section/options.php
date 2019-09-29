81e19621cca7a122575c1fcb0c8571b91edd520a
iproman <itechman@yandex.ru>
2019-09-30 02:16:53 +0500

    Add custom options for unyson plugin.

---
+++ wp-content/themes/clean/framework-customizations/extensions/shortcodes/shortcodes/section/options.php
@@ -0,0 +1,36 @@
+<?php if (!defined('FW')) {
+	die('Forbidden');
+}
+
+$options = array(
+	'is_fullwidth' => array(
+		'label'        => __('Full Width', 'fw'),
+		'type'         => 'switch',
+	),
+	'background_color' => array(
+		'label' => __('Background Color', 'fw'),
+		'desc'  => __('Please select the background color', 'fw'),
+		'type'  => 'color-picker',
+	),
+	'background_image' => array(
+		'label'   => __('Background Image', 'fw'),
+		'desc'    => __('Please select the background image', 'fw'),
+		'type'    => 'background-image',
+		'choices' => array(//	in future may will set predefined images
+		)
+	),
+	'video' => array(
+		'label' => __('Background Video', 'fw'),
+		'desc'  => __('Insert Video URL to embed this video', 'fw'),
+		'type'  => 'text',
+	),
+    // custom
+    'custom_id' => array(
+        'label' => __('Custom ID', 'clean'),
+        'type'  => 'text',
+    ),
+    'custom_class' => array(
+        'label' => __('Custom Class', 'clean'),
+        'type'  => 'text',
+    ),
+);
