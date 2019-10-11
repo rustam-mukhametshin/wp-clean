12ebdb31ada7a4870390675f1057f0640338944f
iproman <itechman@yandex.ru>
2019-10-11 19:00:29 +0500

    Change sidebar.

--- wp-content/themes/clean/functions.php
+++ wp-content/themes/clean/functions.php
@@ -112,16 +112,17 @@ add_action( 'after_setup_theme', 'clean_content_width', 0 );
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function clean_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'clean' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'clean' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
+function clean_widgets_init()
+{
+    register_sidebar(array(
+        'name' => esc_html__('Sidebar', 'clean'),
+        'id' => 'sidebar-1',
+        'description' => esc_html__('Add widgets here.', 'clean'),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget' => '</section>',
+        'before_title' => '<h3 class="widget-title">',
+        'after_title' => '</h3>',
+    ));
 }
 add_action( 'widgets_init', 'clean_widgets_init' );
 
