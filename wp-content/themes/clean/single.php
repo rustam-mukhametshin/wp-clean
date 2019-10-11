ea21415cd163045b8fe91782ce31f2879a9917e4
iproman <itechman@yandex.ru>
2019-10-11 18:59:10 +0500

    Change single layout depends on post_format.

--- wp-content/themes/clean/single.php
+++ wp-content/themes/clean/single.php
@@ -9,28 +9,24 @@
 
 get_header();
 ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', get_post_type() );
-
-			the_post_navigation();
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+    <div id="fh5co-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <div class="row">
+                        <div class="col-md-3 col-md-push-9 animate-box">
+                            <?php get_sidebar(); ?>
+                        </div>
+                        <div class="col-md-9 col-md-pull-3">
+                            <?php while (have_posts()) : the_post(); ?>
+                                <?php get_template_part('template-parts/content', get_post_format()) ?>
+                            <?php endwhile; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <?php
 get_sidebar();
